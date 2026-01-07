9ca1e23555759b10bb6636a2aad470d22ea65762
ohsung0722 <99doldol@naver.com>
2026-01-07 16:07:48 +0900

    feat: controller 초기 세팅

---
+++ src/controller/ScheduleController.js
@@ -0,0 +1,17 @@
+import InputView from "../view/InputView.js";
+import OutputView from "../view/OutputView.js";
+
+class Controller {
+  #inputView;
+  #outputView;
+  constructor() {
+    this.#inputView = new InputView();
+    this.#outputView = new OutputView();
+  }
+
+  async controlSchedule() {
+    this.#outputView.printStartMessage();
+  }
+}
+
+export default Controller;
